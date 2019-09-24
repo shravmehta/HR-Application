c06a7f3dcde551c09317b5d8e0bcc17023783052
shrav mehta <shravmehta@shravs-MacBook-Air.local>
2019-09-24 18:14:25 -0400

    added menu and componenets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'add',
+    component: AddComponent
+  },
+  {
+    path:'list',
+    component: ListComponent
+  },
+  {
+    path: '*',
+    component: ListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AddComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
