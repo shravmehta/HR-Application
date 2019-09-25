e23df2b03a7aabe034080fc933511d6321a10b97
shrav mehta <shravmehta@shravs-MacBook-Air.local>
2019-09-24 20:17:27 -0400

    added employees lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { AddComponent } from './add/add.component';
 import { ListComponent } from './list/list.component';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  public employeeList =[];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('http://167.71.80.155:3000/employees')
+    .subscribe(resp =>{
+      this.employeeList = resp['response']
+    });
   }
 
 }
